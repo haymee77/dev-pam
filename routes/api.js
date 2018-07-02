d6183ef34ea5f7b77a4d139664f941b82023120d
haymee77 <hmpark0502@gmail.com>
2018-07-03 01:22:27 +0900

    api test delete user

--- routes/api.js
+++ routes/api.js
@@ -3,43 +3,64 @@ var router = express.Router();
 
 // api test
 let users = [
-  {
-    id: 1,
-    name: 'Rachel'
-  },
-  {
-    id: 2,
-    name: 'Monica'
-  },
-  {
-    id: 3,
-    name: 'Pheebe'
-  }
+    {
+        id: 1,
+        name: 'Rachel'
+    },
+    {
+        id: 2,
+        name: 'Monica'
+    },
+    {
+        id: 3,
+        name: 'Pheebe'
+    }
 ];
 
 /* GET users listing. */
+router.get('/users', (req, res) => {
+    return res.json(users);
+});
+
 router.get('/users/:id', (req, res) => {
   
-  const id = parseInt(req.params.id, 10);
+    const id = parseInt(req.params.id, 10);
 
-  if (!id) {
-    return res.status(400).json({error: 'Incorrect id'});
-  }
+    if (!id) {
+        return res.status(400).json({error: 'Incorrect id'});
+    }
 
-  let user = users.filter(user => {
-      return user.id === id;
-  });
+    let user = users.filter(user => {
+        return user.id === id;
+    });
 
-  if (!user[0]) {
-    return res.status(404).json({errro: 'Unknown user'});
-  }
+    if (!user[0]) {
+        return res.status(404).json({errro: 'Unknown user'});
+    }
 
-  return res.json(user[0]);
+    return res.json(user[0]);
   
 });
 
-router.get('/users', (req, res) => {
-  return res.json(users);
+router.delete('/users/:id', (req, res) => {
+
+    const id = parseInt(req.params.id, 10);
+
+    if (!id) {
+        return res.status(400).json({error: 'Incorrect id'});
+    }
+
+    const userIdx = users.findIndex(user => {
+        return user.id === id;
+    });
+
+    if (userIdx === -1) {
+        return res.status(404).json({errro: 'Unknown user'});
+    }
+
+    users.splice(userIdx, 1);
+    return res.status(204).send();
+
 });
 
 module.exports = router;

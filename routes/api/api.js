4eb5977c9db9563972f36c9564005a471702128d
haymee77 <hmpark0502@gmail.com>
2018-07-15 21:47:29 +0900

    db sync test

--- routes/api/api.js
+++ routes/api/api.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/users', require('./users'));
+router.use('/users', () => {
+    require('./users');
+    console.log('Pam listening on port 3000');
+  
+    require('../../models').sequelize.sync({force: true})
+    .then(() => {
+        console.log('Databases sync');
+    });
+});
 
 module.exports = router;

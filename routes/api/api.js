e5e52f4fb307bd2753bfe25ca6803f167ea2eb39
haymee77 <hmpark0502@gmail.com>
2018-07-15 23:04:23 +0900

    revert db test

--- routes/api/api.js
+++ routes/api/api.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/users', () => {
-    require('./users');
-    console.log('Pam listening on port 3000');
-  
-    require('../../models').sequelize.sync({force: true})
-    .then(() => {
-        console.log('Databases sync');
-    });
-});
+router.use('/users', require('./users'));
 
 module.exports = router;

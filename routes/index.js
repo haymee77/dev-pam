c644999b24f5a01f8b8d9ead6fa4a3375ad00696
Boseok, Seo <ggy7bbo@gmail.com>
2018-07-30 08:36:44 +0900

    Angular route fix for Not found problem

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/*", function(req, res) {
+  // Router => Angular (Front-end)
+  res.sendFile(path.join(__dirname, "views/index.html"), function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 module.exports = router;
